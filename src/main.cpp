d3dc28814b83f461d6f137fdbbbe0f95c6575cc6
Umang Sakhare <umang1715@gmail.com>
2025-05-31 02:43:55 +0100

    Added accommodation to create persistance in bg

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <string>
-#include "../include/RedisServer.h""
+#include "../include/RedisServer.h"
 #include "RedisServer.cpp"
+#include <thread>
+#include <chrono>
 
 int main(int argc, char* argv[]) {
     int port = 6379; //default
     if (argc>=2) port = std::stoi(argv[1]);
 
     RedisServer server(port);
+
+    // Create background persistance. Save contents to database every 300 seconds.
+    std::thread persistanceThread([](){
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(300));
+            // Dump the database
+        }
+    });
+    persistanceThread.detach();
+
+    server.run();
+
     return 0;
 }
